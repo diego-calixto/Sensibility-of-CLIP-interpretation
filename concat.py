c23eae0e598c86cc03d76e4828e76de18c28fca0
Diego Calixto <diegocalixto60@gmail.com>
2025-07-01 23:35:30 -0300

    8/255 metrics

---
+++ concat.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+
+# Caminho onde os arquivos de batches estão localizados
+batch_dir = "./"
+batch_files = [f for f in os.listdir(batch_dir) if f.startswith('results_batch')]
+
+batch_files_sorted = sorted(batch_files, key=lambda x: int(x.split('_')[2].split('.')[0]))
+
+# Inicializar uma lista para armazenar os DataFrames
+dfs = []
+
+# Ler e concatenar todos os arquivos de batches
+for batch_file in batch_files_sorted:
+    batch_df = pd.read_pickle(os.path.join(batch_dir, batch_file))
+    dfs.append(batch_df)
+
+
+# Concatenar todos os DataFrames em um só
+final_df = pd.concat(dfs, ignore_index=True)
+
+# Salvar o DataFrame completo em um único arquivo
+final_df.to_pickle("./results_full.pkl")
+
+# Confirmar a conclusão
+print("Todos os batches foram combinados em um único arquivo!")
